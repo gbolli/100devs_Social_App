387c03ff879b3a6d0cefb6baa94c8b5cad6aaa48
Gord Bolli <gord@bolli.ca>
2022-09-22 14:31:02 -0700

    Add comments to posts.   Comment model, route, post controller, post view

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../models/Post')
+const Comment = require('../models/Comment')
 const cloudinary = require('../middleware/cloudinary')
 
 module.exports = {
@@ -24,7 +25,10 @@ module.exports = {
     getPost: async (req, res) => {
         try {
             const post = await Post.findById(req.params.id)
-            res.render('post.ejs', { post: post, user: req.user })
+            const comments = await Comment
+                .find({ post: req.params.id })
+                .sort({ createdAt: 'desc' }).lean()
+            res.render('post.ejs', { post: post, comments: comments, user: req.user })
         } catch (error) {
             console.error(error)
         }
@@ -44,6 +48,20 @@ module.exports = {
             })
             console.log('Post has been added!')
             res.redirect('/profile')
+        } catch (error) {
+            console.error(error)
+        }
+    },
+    createComment: async (req, res) => {
+        try {
+            await Comment.create({
+                comment: req.body.comment,
+                post: req.params.id,
+                user: req.user.id,
+                likes: 0
+            })
+            console.log('Comment has been added')
+            res.redirect('/post/'+req.params.id)
         } catch (error) {
             console.error(error)
         }
