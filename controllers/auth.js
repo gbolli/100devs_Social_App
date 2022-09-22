0ee8109ac65ee52790a1b85cc436cf4086c041a4
Gord Bolli <gord@bolli.ca>
2022-09-21 22:42:41 -0700

    Add validator to login and signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const User = require('../models/User')
+const validator = require('validator')
 
 
 module.exports = {
@@ -21,24 +22,56 @@ module.exports = {
         })
     },
     postLogin: (req, res, next) => {
+        const validationErrors = []
+
+        if (!validator.isEmail(req.body.email))
+            validationErrors.push({ msg: "Please enter a valid email address." })
+        if (validator.isEmpty(req.body.password))
+            validationErrors.push({ msg: "Password cannot be blank." })
+
+        if (validationErrors.length) {
+            req.flash("errors", validationErrors)
+            return res.redirect("/login")
+        }
+        req.body.email = validator.normalizeEmail(req.body.email, {
+            gmail_remove_dots: false,
+        })
+
         passport.authenticate("local", (err, user, info) => {
             if (err) {
-              return next(err)
+                return next(err)
             }
             if (!user) {
-              req.flash("errors", info)
-              return res.redirect("/login")
+                req.flash("errors", info)
+                return res.redirect("/login")
             }
             req.logIn(user, (err) => {
-              if (err) {
-                return next(err)
-              }
-              req.flash("success", { msg: "Success! You are logged in." })
-              res.redirect(req.session.returnTo || "/profile")
+                if (err) {
+                    return next(err)
+                }
+                req.flash("success", { msg: "Success! You are logged in." })
+                res.redirect(req.session.returnTo || "/profile")
             })
-          })(req, res, next)
+        })(req, res, next)
     },
     postSignup: (req, res, next) => {
+        const validationErrors = []
+
+        if (!validator.isEmail(req.body.email))
+            validationErrors.push({ msg: "Please enter a valid email address." })
+        if (!validator.isLength(req.body.password, { min: 8 }))
+            validationErrors.push({ msg: "Password must be at least 8 characters long" })
+        if (req.body.password !== req.body.confirmPassword)
+            validationErrors.push({ msg: "Passwords do not match" })
+
+        if (validationErrors.length) {
+            req.flash("errors", validationErrors)
+            return res.redirect("../signup")
+        }
+        req.body.email = validator.normalizeEmail(req.body.email, {
+            gmail_remove_dots: false,
+        })
+
         const user = new User({
             userName: req.body.userName,
             email: req.body.email,
